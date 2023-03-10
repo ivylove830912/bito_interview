80d958d6e0cfbf4547f6d0c7761dbc0eed561a2e
Ivy <39575608+ivylove830912@users.noreply.github.com>
2023-03-10 21:52:43 +0800

    Delete Order.py

--- Order.py
+++
@@ -1,8 +0,0 @@
-def order(sentence):
-    string = []
-    for i in range(1, 10):
-        for word in sentence.split():
-            if str(i) in word:
-                string.append(word)
-
-    return " ".join(string)
